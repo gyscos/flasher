3f2eec14df32da8ca421eaa4bc1239eb0665e621
Alexandre Bury <alexandre.bury@gmail.com>
2017-09-26 00:19:53 -0700

    Add Readme

---
+++ software/Readme.md
@@ -0,0 +1,10 @@
+This is the software side of the flasher project.
+
+It includes:
+* A set of trigger scripts (`main.sh`, `format.sh`, `backup.sh`) that perform backup or filesystem format actions when called.
+* A systemd service file (`flasher-trigger@.service`) that calls `main.sh` with the given argument (this is mostly used as a workaroung to bypass the limitations of udev).
+* A udev rule (`90-flasher.rules`) to start the `flasher-trigger` service whenever a flash drive is plugged.
+* A python daemon (`flasherdaemon.py`) that serves as an interface between the trigger script and the GPIO LEDs and buttons (currently only work on raspberry pi).
+* A systemd service file (`flasher.service`) that runs the python daemon.
+
+You can package everything in a `pkg` directory by running `DESTDIR=pkg/ ./install.sh`. There is also a `PKGBUILD` and a `flasher.install` to build an Archlinux package.
--- software/install.sh
+++ software/install.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 cd $(dirname "$0")
-mkdir -p "$PREFIX/usr/lib/systemd/system"
-mkdir -p "$PREFIX/etc/udev/rules.d"
-mkdir -p "$PREFIX/usr/lib/flasher"
-mkdir -p "$PREFIX/usr/bin"
-mkdir -p "$PREFIX/var/lib/flasher/backups"
-mkdir -p "$PREFIX/var/lib/flasher/state"
+mkdir -p "$DESTDIR/usr/lib/systemd/system"
+mkdir -p "$DESTDIR/etc/udev/rules.d"
+mkdir -p "$DESTDIR/usr/lib/flasher"
+mkdir -p "$DESTDIR/usr/bin"
+mkdir -p "$DESTDIR/var/lib/flasher/backups"
+mkdir -p "$DESTDIR/var/lib/flasher/state"
 
-cp flasher-trigger@.service "$PREFIX/usr/lib/systemd/system/"
-cp flasher.service "$PREFIX/usr/lib/systemd/system"
+cp flasher-trigger@.service "$DESTDIR/usr/lib/systemd/system/"
+cp flasher.service "$DESTDIR/usr/lib/systemd/system"
 
-cp 90-flasher.rules "$PREFIX/etc/udev/rules.d/"
+cp 90-flasher.rules "$DESTDIR/etc/udev/rules.d/"
 
-cp backup.sh "$PREFIX/usr/lib/flasher/"
-cp format.sh "$PREFIX/usr/lib/flasher/"
-cp flasherdaemon.py "$PREFIX/usr/lib/flasher/"
+cp backup.sh "$DESTDIR/usr/lib/flasher/"
+cp format.sh "$DESTDIR/usr/lib/flasher/"
+cp flasherdaemon.py "$DESTDIR/usr/lib/flasher/"
 
-cp main.sh "$PREFIX/usr/bin/flasher"
+cp main.sh "$DESTDIR/usr/bin/flasher"
